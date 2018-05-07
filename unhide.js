07705f2a3d99e44f56b1066557cedbcae7e45f9f
Liam D <liamdocuk@gmail.com>
2018-05-08 00:44:38 +0200

    Update unhide.js

--- unhide.js
+++ unhide.js
@@ -1,12 +1,9 @@
 
 function unhide() {
-	console.log('This is working')
-	
 	document.querySelector("div.tp-modal").style.display = "none";
 	document.querySelector("div.tp-backdrop.tp-active").style.display = "none";
 	document.querySelector("body").style.cssText = "overflow: scroll !important";
 	document.querySelector("div#fade-box").style.backgroundImage = "none";
 };
-
 
 setTimeout(unhide, 4000)
